59a807b359de02042c0457ea9f9b7d8950a84c51
czapponi <littleark@gmail.com>
2015-12-01 23:15:18 +0000

    calculation, labels

--- src/js/components/Flow.js
+++ src/js/components/Flow.js
@@ -10,12 +10,14 @@ export default class Flow {
 		let search=window.location.search.replace(/\?/gi,""),
 			rankings=this.options.ranking.map((d)=>d.country);
 
-		//console.log(search,rankings)
+		////console.log(search,rankings)
 		this.COUNTRY=rankings.find((d)=>{
 			return d.toLowerCase() == search.toLowerCase();
 		});
 		this.options.country=this.COUNTRY;
 		//alert(this.COUNTRY)
+		console.log(this.COUNTRY)
+
 
 		this._buildCountrySelector();
 		this._buildRestartCountrySelector();
@@ -33,51 +35,70 @@ export default class Flow {
 	_buildCharts(index,info) {
 		let self=this;
 
-		console.log("_buildCharts",index)
+		
 
 		index++;
+		//console.log("_buildCharts",index)
 		this.currentQuestion=index;
 
 		if(!this.currentQuestion) {
 			
+			this.options.questions=this.options.questions.filter((d,i)=>{
+						//console.log(d,this.data,this.options.country)
+						let dd=this.data.find((c)=>c.country===this.options.country),
+							qq=dd.questions.find((q)=>q.question===d.id);
+						////console.log(qq)
+
+						return !isNaN(qq.mean);
+
+						return 1;
+						return d==="1";
+			}).sort((a,b)=>{
+					return a.index-b.index
+				})
+
+
 			this.options.question=this.options.questions[index].id;
 			this.options.nextCallback=function(d,info){
 				self._buildCharts(d,info)
 			};
 
-			//console.log("1 currentQuestion",this.options.question.id)
+			////console.log("1 currentQuestion",this.options.question.id)
 
 			this.pc=new ParallelCoordinates(this.data,this.options);
 		} else {
 			
-			console.log(index,this.options.questions.length)
+			//console.log(index,this.options.questions.length)
 			info["difference (mean-actual)"]=info.mean-info.actual;
-			console.log("INFOOOOO",info)
+			//console.log("INFOOOOO",info)
 			this.user.questions.push(info);
-			console.log("USER USER USER",this.user)
-			//console.log("2 currentQuestion",this.options)
+			//console.log("USER USER USER",this.user)
+			////console.log("2 currentQuestion",this.options)
+
+			//console.log(this.options.questions)
+
 			if(index<this.options.questions.length){// && index < 2){
 
 				this.options.question=this.options.questions[index].id;
-				console.log(this.options)
-				this.pc.nextQuestion(this.options.question);	
+				//console.log(this.options)
+				this.pc.nextQuestion(this.options.question);
 			} else {
-				console.log("DONE!!!! USER USER USER",this.user)
+				//console.log("DONE!!!! USER USER USER",this.user)
 				let obj={
 					answers:{
 						country:this.user.country,
 						q:this.user.questions.map((d)=>d.mean)
 					}
 				};
-				console.log(obj)
+				//console.log(obj)
 
 				this._buildRanking();
-				console.log("REMIND TO ACTIVATE XHR")
+				//console.log("REMIND TO ACTIVATE XHR")
 				return;
 				d3.xhr("http://ec2-54-72-6-69.eu-west-1.compute.amazonaws.com/")
 					.header("Content-Type", "application/json")
 					.post(JSON.stringify(obj),function(error,data){
-						//console.log("SEND!")
+						////console.log("SEND!")
 					})
 			}
 			
@@ -92,7 +113,7 @@ export default class Flow {
 										.on("click",function(){
 											d3.event.preventDefault();
 											if(self.COUNTRY) {
-												console.log(self.COUNTRY)
+												//console.log(self.COUNTRY)
 
 												self.user.country=self.COUNTRY;
 
@@ -107,7 +128,7 @@ export default class Flow {
 
 		let countries=d3.select("#countrySelector")
 						.on("change",function(d){
-							//console.log()
+							////console.log()
 							self.COUNTRY=this.options[this.selectedIndex].value;
 							self.options.country=self.COUNTRY;
 
@@ -115,7 +136,11 @@ export default class Flow {
 
 						})
 						.selectAll("option")
-						.data(this.data.map((d)=>d.country))
+						.data(this.data.map((d)=>d.country).sort((a,b)=>{
+							if(a < b) return -1;
+						    if(a > b) return 1;
+						    return 0;
+						}))
 						.enter()
 						.append("option")
 							.attr("value",(d)=>d)
@@ -135,9 +160,9 @@ export default class Flow {
 		let confirm_button=d3.select("button#restartCountry")
 										.on("click",function(){
 											d3.event.preventDefault();
-											console.log(window.location)
+											//console.log(window.location)
 											if(COUNTRY) {
-												console.log(window.location.origin+window.location.pathname+"?"+COUNTRY)
+												//console.log(window.location.origin+window.location.pathname+"?"+COUNTRY)
 
 												let url=window.location.origin+window.location.pathname+"?"+COUNTRY;
 
@@ -150,7 +175,7 @@ export default class Flow {
 
 		let countries=d3.select("#restartCountrySelector")
 						.on("change",function(d){
-							//console.log()
+							////console.log()
 							COUNTRY=this.options[this.selectedIndex].value;
 							
 
@@ -168,16 +193,19 @@ export default class Flow {
 
 	_buildRanking(){
 		
+		console.log("BUILD RANKING",this.user)
 
 
 		let avg={
 				country:"YOU",
 				avg:d3.mean(this.user.questions,(d)=>Math.abs(d.mean-d.actual)),
-				standardized:d3.mean(this.user.questions,(q)=>{
+				adj:this.user.questions.map((q)=>{
 	                let question=this.options.questions.find((qq)=>{
-	                    return (qq.id===q.question);
+	                	//console.log(q,qq)
+	                    return (qq.id===q.question.id);
 	                });
 	                if(question) {
+	                	//console.log(question)
 	                    let accuracy_score=(Math.abs(q.mean-q.actual)),
 	                        Pm = (q.mean+q.actual)/2,
 	                        sdm = Math.sqrt(Pm * (100-Pm)),
@@ -187,12 +215,19 @@ export default class Flow {
 	                        st_acc_score=accuracy_score;
 	                    }
 	                    
-	                    return accuracy_score;
+	                    return st_acc_score;
 	                }
 	                return 0;
 	            })
-			},
-			ranking=this.options.ranking.concat([avg]);
+			};
+
+		console.log(avg)
+		let ranking=this.options.ranking.concat([
+			{
+				country:"YOU",
+				adj:Math.round(d3.sum(avg.adj)*(122/171.85))
+			}
+		]);
 
 
 
@@ -200,7 +235,7 @@ export default class Flow {
 			.classed("hidden",false)
 			.select("ul")
 			.selectAll("li")
-				.data(ranking.sort((a,b)=>(a.standardized-b.standardized)))
+				.data(ranking.sort((a,b)=>(a.adj-b.adj)))
 				.enter()
 				.append("li")
 					.attr("class",(d)=>{
@@ -208,7 +243,7 @@ export default class Flow {
 					})
 					.classed("you",(d)=>d.country==="YOU")
 					.classed("selected",(d)=>d.country===this.options.country)
-					.html((d,i)=>("<span>"+(i<10?"0":"")+(i+1)+".</span> "+d.country))
+					.html((d,i)=>("<span>"+(i<10?"0":"")+(i+1)+".</span> "+d.country+":"+d.raw+","+d.adj))
 	}
 
 	_getCountryArea(country) {
@@ -229,8 +264,8 @@ export default class Flow {
 			"005":"samerica",
 			"013":"samerica"
 		}
-		//console.log(country)
-		//console.log(region_codes[this.options.country_info[country]["region-code"]])
+		////console.log(country)
+		////console.log(region_codes[this.options.country_info[country]["region-code"]])
 
 		if(sub_region_codes[this.options.country_info[country]["sub-region-code"]]) {
 			return sub_region_codes[this.options.country_info[country]["sub-region-code"]]
